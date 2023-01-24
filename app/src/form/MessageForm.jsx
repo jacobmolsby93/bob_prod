796f000198f1cbe671488e8875adb0dc24ba83b7
jacobmolsby93 <jacobmolsby@hotmail.se>
2023-01-24 01:55:54 +0100

    hello

--- app/src/form/MessageForm.jsx
+++ app/src/form/MessageForm.jsx
@@ -33,6 +33,7 @@ const MessageFields = () => {
   const templateId = import.meta.env.VITE_TEMPLATE_ID
   if(process.env.NODE_ENV === "production") {
     console.log(process.env.VITE_SERVICE_ID)
+    console.log(import.meta.env.VITE_PUBLIC_KEY)
   }
 
   const handleClose = () => {
