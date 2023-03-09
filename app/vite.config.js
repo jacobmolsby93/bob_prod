e03ab973a311c0d644e08405c1cb8e04c9c6d3dc
jacobmolsby93 <jacobmolsby@hotmail.se>
2023-03-09 19:53:30 +0100

    Add caching

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import VitePWA from 'vite-plugin-pwa';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   entry: "src/main.js",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'my-sw.js',
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,png,jpg,jpeg,gif,svg}',
+        ],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/example\.com\/api/,
+            handler: 'networkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 7, // cache for a week
+              },
+              networkTimeoutSeconds: 10, // wait for 10 seconds before failing
+            },
+          },
+          {
+            urlPattern: /^https:\/\/example\.com\/static/,
+            handler: 'cacheFirst',
+            options: {
+              cacheName: 'static-cache',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365, // cache for a year
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
