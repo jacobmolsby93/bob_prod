16899181aaf4a342ae521659f3b977a6d0e2dcd0
jacobmolsby93 <jacobmolsby@hotmail.se>
2023-03-09 20:48:54 +0100

    up

--- app/workbox-config.js
+++ app/workbox-config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    swDest: 'dist/service-worker.js',
-    globDirectory: 'dist/',
-    globPatterns: [
-      '**/*.{html,json,js,css}',
-      'images/**/*.{jpg,jpeg,png,webp,svg,gif}'
-    ]
-  };
+  swDest: 'dist/service-worker.js',
+  swSrc: 'src/sw.js', 
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*.{html,json,js,css}',
+    'images/**/*.{jpg,jpeg,png,webp,svg,gif}'
+  ]
+};
